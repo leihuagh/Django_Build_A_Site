81dde916c5a900471371f8d7e9b67b48559a8c20
leihua <leihua@masslottery.com>
2019-01-19 17:46:41 -0500

    corrected contact layout

--- common/views.py
+++ common/views.py
@@ -10,4 +10,9 @@ def about(request):
 
 
 def contact(request):
+    if request.method == 'POST':
+        print(request.POST)
+        print(request.POST.get('fullname'))
+        print(request.POST.get('email'))
+        print(request.POST.get('content'))
     return render(request, 'contact.html', {})
