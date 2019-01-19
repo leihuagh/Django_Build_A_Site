37f67fab300c442b07b3bb0cd002ed5f39ce81b6
leihua <leihua@masslottery.com>
2019-01-18 23:12:09 -0500

    static files

--- config/settings.py
+++ config/settings.py
@@ -113,3 +113,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = [
+    STATIC_DIR
+]
