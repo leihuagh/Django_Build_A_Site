830799be93fd11381b568f5c7921d97f1dde7414
leihua <leihua@masslottery.com>
2019-01-19 09:52:10 -0500

    contact basic page

--- common/urls.py
+++ common/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import index, about
+from .views import index, about, contact
 
 urlpatterns = [
     path('', index, name='index'),
-    path('about/', about, name='about')
+    path('about/', about, name='about'),
+    path('contact/', contact, name='contact'),
 ]
