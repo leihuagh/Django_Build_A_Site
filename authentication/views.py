83b69f4230fe3827cf58b8eb313dd41194b5094e
leihua <leihua@masslottery.com>
2019-01-20 20:49:06 -0500

    setup logout page

--- authentication/views.py
+++ authentication/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def login_view(request):
     return render(request, "auth/login.html", {})
+
+
+def logout_view(request):
+    return render(request, "auth/logout.html", {})
--- common/urls.py
+++ common/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from .views import index, about, contact
-from authentication.views import login_view
+from authentication.views import login_view, logout_view
 
 urlpatterns = [
     path('', index, name='index'),
     path('about/', about, name='about'),
     path('contact/', contact, name='contact'),
-    path('login/', login_view, name='login')
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
 ]
