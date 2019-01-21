1737e414a3a42785997f740211b5e4c643056311
leihua <leihua@masslottery.com>
2019-01-20 20:16:05 -0500

    create basic login

--- common/urls.py
+++ common/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import index, about, contact
+from authentication.views import login_view
 
 urlpatterns = [
     path('', index, name='index'),
     path('about/', about, name='about'),
     path('contact/', contact, name='contact'),
+    path('login/', login_view, name='login')
 ]
