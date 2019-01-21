a7a33e66e90b8cd26d3a9e64836e642a4aa778ac
leihua <leihua@masslottery.com>
2019-01-21 09:27:55 -0500

    login form

---
+++ authentication/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Username"
+            }
+        )
+    )
+
+    password = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control"
+            }
+        )
+    )
--- authentication/views.py
+++ authentication/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
+from .forms import LoginForm
 
 
 def login_view(request):
-    return render(request, "auth/login.html", {})
+    form = LoginForm(request.POST or None)
+    if form.is_valid():
+        print(form.cleaned_data)
+    context = {
+        "form": form
+    }
+    return render(request, "auth/login.html", context)
 
 
 def logout_view(request):
