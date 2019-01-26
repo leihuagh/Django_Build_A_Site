783f4fa6c90b68304be9b81c0d136e83e3ec1241
leihua <leihua@masslottery.com>
2019-01-26 00:46:26 -0500

    product model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from core.models import TimeStampModel
 
-# Create your models here.
+
+class Product(TimeStampModel):
+    title = models.CharField(max_length=120)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=39.99)
+
+    def __str__(self):
+        return self.title
