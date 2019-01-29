79b8fa85b454ff06555ade5a46281703a2cbfd24
leihua <leihua@masslottery.com>
2019-01-28 20:57:42 -0500

    setup static

--- config/urls.py
+++ config/urls.py
@@ -12,4 +12,5 @@ urlpatterns = [
 ]
 
 urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
