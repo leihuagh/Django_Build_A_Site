61c75ba1bc3e99ac6501e3d0d2229423b72d00aa
leihua <leihua@masslottery.com>
2019-01-30 22:27:50 -0500

    customize product admin

--- products/admin.py
+++ products/admin.py
@@ -1,4 +1,16 @@
 from django.contrib import admin
 from .models import Product
 
-admin.site.register(Product)
+
+class ProductAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created'
+    search_fields = ['__str__', 'description', 'price']
+    list_display = ['__str__', 'description', 'price', 'created', 'updated']
+    list_editable = ['price']
+    readonly_fields = ['created', 'updated']
+
+    class Meta:
+        model = Product
+
+
+admin.site.register(Product, ProductAdmin)
