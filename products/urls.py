78a1093d31e75bef920951d0740f611490936ba1
leihua <leihua@masslottery.com>
2019-01-30 21:06:36 -0500

    retrieve all products

--- products/urls.py
+++ products/urls.py
@@ -3,5 +3,5 @@ from .views import list_view
 
 app_new = 'products'
 urlpatterns = [
-    path('list/', list_view, name='list_view'),
+    path('', list_view, name='list'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 from .models import Product
+
+class ProductListView(ListView):
+    queryset = Product.objects.all()
+    template_name = 'list.html'
 
 
 def list_view(request):
-    queryset = Product.objects.all()
+    queryset = Product.objects.all().order_by('created')
+    print(queryset)
     context = {
-        "objects": queryset
+        "products": queryset,
     }
-
-    return render(request, 'products/list.html', context)
+    return render(request, 'list.html', context)
